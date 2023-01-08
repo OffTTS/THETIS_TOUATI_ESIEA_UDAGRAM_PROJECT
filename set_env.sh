ad5f232fefdab292c5fd6028c31bf8ddfbc409bf
Clement Thetis <clement.thetis@gmail.com>
2023-01-08 22:16:27 +0100

    esiea-4th-vcc-project

--- set_env.sh
+++ set_env.sh
@@ -2,12 +2,12 @@
 
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=admin
+export POSTGRES_USERNAME=admintest
 export POSTGRES_PASSWORD=admin1234
-export POSTGRES_HOST=udagramd-db
-export POSTGRES_DB=udagramdb
-export AWS_BUCKET=udagram-esiea-8765
-export AWS_REGION=us-east-1
+export POSTGRES_HOST=udagram-clem.cryhrvawhe5a.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=udagram-clem
+export AWS_BUCKET=arn:aws:s3:::clembucket7
+export AWS_REGION=us-east-2b
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
